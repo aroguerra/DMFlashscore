f07fc4341a591d9c1b6d6fdabaa22b89f73f7461
Abraham Guerra <abraham.rguerra@gmail.com>
2023-12-01 17:28:28 +0200

    program fetches 5 last seasons and performance of the last 5 matches and prints it

--- main.py
+++ main.py
@@ -2,39 +2,57 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-url = 'https://www.flashscore.com/standings/jDTEm9zs/I3O5jpB2/#/I3O5jpB2/table/overall'  # Replace this with the target website URL
-
-driver = webdriver.Chrome()
-driver.get(url)
-
-
-modified_html = driver.page_source
-time.sleep(10)
-print(modified_html)
-team_elements = driver.find_elements(By.CLASS_NAME, 'ui-table__row')
-
-for team_element in team_elements:
-    team_name = team_element.text
-    print("Team Name:", team_name)
+season_urls = ['https://www.flashscore.com/standings/jDTEm9zs/I3O5jpB2/#/I3O5jpB2/table/overall',
+               'https://www.flashscore.com/standings/nmP0jyrt/nunhS7Vn/#/nunhS7Vn/table/overall',
+               'https://www.flashscore.com/standings/tdkpynmB/6kJqdMr2/#/6kJqdMr2/table/overall',
+               'https://www.flashscore.com/standings/AJuiuwWt/zTRyeuJg/#/zTRyeuJg/table/overall',
+               'https://www.flashscore.com/standings/h2NtrDMq/CxZEqxa7/#/CxZEqxa7/table/overall'
+               ]
 
 
+def get_season_info(season_url):
+    driver = webdriver.Chrome()  # Create a new WebDriver instance for each task
+    # modified_html = driver.page_source
+    # time.sleep(20)  # time to process the page
+
+    try:
+        driver.get(season_url)  # Perform Selenium interactions
+        time.sleep(20)
+
+        season = driver.find_element(By.CLASS_NAME, 'dropdown__selectedValue')
+        print(f'SEASON: {season.text}')
+        standings_table = driver.find_elements(By.CLASS_NAME, 'ui-table__row')
+        if '2024' in season.text:
+            anchor = driver.find_element(By.XPATH, '//a[contains(@href, "/form")]')  # button form
+            get_team_form_5matches(anchor)
+
+        for team_elements in standings_table:
+            team_stats = team_elements.text.split('\n')
+
+            print(f"Position:{team_stats[0]}")
+            print(
+                f"Team Name:{team_stats[1]} matches:{team_stats[2]} W:{team_stats[3]} D:{team_stats[4]} L:{team_stats[5]} GD:{team_stats[6]}")
+    except Exception as er:
+        print(f'something went wrong {er}')
+    finally:
+        driver.quit()  # Close the WebDriver instance
 
 
+def get_team_form_5matches(anchor):
+    href_value = anchor.get_attribute('href')
+    driver2 = webdriver.Chrome()
+    driver2.get(href_value)
+    time.sleep(10)
+    print('LAST 5 MATCHES')
+    standings_table = driver2.find_elements(By.CLASS_NAME, 'ui-table__row')
+    for team_elements in standings_table:
+        team_stats = team_elements.text.split('\n')
+
+        print(f"Position:{team_stats[0]}")
+        print(
+            f"Team Name:{team_stats[1]} matches:{team_stats[2]} W:{team_stats[3]} D:{team_stats[4]} L:{team_stats[5]} GD:{team_stats[6]}")
 
 
-
-# if response.status_code == 200:
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     # print(soup.prettify())
-#     # title_elements = soup.select('.titleColumn a')
-#     main_element = soup.find("main", class_="container__liveTableWrapper tournament_page", id="mc")
-#     inner_divs = main_element.find_all('div', class_='container__fsbody', id="fsbody")
-#     inner_divs_2 = main_element.find_all('div', class_='sport-soccer', id="tournament-table")
-#     inner_divs_3 = main_element.find_all('div', class_='ui-table__body')
-#     # mydivs = soup.find_all("div", {"class":"container__liveTableWrapper tournament_page"})
-#     for title_element in inner_divs_3:
-#         print(title_element)
-#
-#     # print(response.text)  # Print the content of the response
-# else:
-#     print(f'Request failed with status code: {response.status_code}')
+# Define a list of URLs for tasks
+for url in season_urls:
+    get_season_info(url)
