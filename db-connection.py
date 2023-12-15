3cb9f20bc4c5e91f7031ff3ba16873ce4ad992ea
Abraham Guerra <abraham.rguerra@gmail.com>
2023-12-15 10:16:35 +0200

    db connection done and also Creates database flashscore from file. It inserts some testing values

---
+++ db-connection.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+# Set your MySQL connection parameters
+host = 'localhost'
+user = 'root'
+password = '********'
+database = 'flashscore'  # Assuming your database is named 'flashscore'
+
+sql_file_path = 'flash.sql'
+
+try:
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password
+    )
+
+    if connection.is_connected():
+        print(f'Connected to MySQL database: {database}')
+
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+
+        # Read and execute SQL statements from the file
+        with open(sql_file_path, 'r') as sql_file:
+            sql_statements = sql_file.read()
+
+        # Split SQL statements into individual queries
+        queries = sql_statements.split(';')
+
+        # Execute each query one by one
+        for query in queries:
+            if query.strip():
+                cursor.execute(query)
+
+        # Commit the changes
+        connection.commit()
+
+        print('SQL file executed successfully.')
+
+        # Close the cursor and connection
+        cursor.close()
+
+except mysql.connector.Error as e:
+    print(f'Error: {e}')
+
+finally:
+    if connection.is_connected():
+        connection.close()
+        print('MySQL connection closed.')
